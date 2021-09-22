3a0164fe9d5bc2ac2437bb5cd2d91676e23c2ef6
Lindembergson <lindembergsonkula@hotmail.com>
2021-09-21 22:18:00 -0300

    Terminando o processo

---
+++ main.js
@@ -0,0 +1,57 @@
+//var nome = "lindem";
+//var idade = 28;
+//var idade2 = 2;
+//var frase = "Japão é o melhor time do mundo";
+//alert(idade + idade2 +" anos");
+
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+/*var n1 = 4;
+var n2 = 5;
+
+console.log(n1 * n2);*/
+//arry
+
+//var lista =[ "maça", "pera", "uva"];
+
+//lista.push()
+//lista.pop()
+//console.log(lista.length);
+//console.log(lista[1]);
+//console.log(lista.reverse());
+
+//var fruta = [{nome: "amizade", tipo:"divino"}, {nome: "maldade", tipo:"cruel"}]
+//console.log(fruta[1].nome);
+
+/*var idade = prompt("Qual é a sua idade?")
+//var idade = 18;
+if(idade>=18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+/*var caunt = 0
+while (caunt <= 100) {
+    console.log(caunt);
+    caunt = caunt + 1;
+}*/
+
+/*var caunt;
+for(caunt=0; caunt <= 5; caunt++) {
+    console.log(caunt);
+};*/
+
+/*var d = new Date();
+alert(d.getMinutes());*/
+
+/*function soma(n1,n2){
+    return(n1+n2);
+}
+alert(soma(10,3));*/
+
+/*function setReplace(frase,nome, novo_nome){
+    return frase.replace(nome,novo_nome);
+}
+
+alert(setReplace("vai Japão","Japão", "USA"));*/
